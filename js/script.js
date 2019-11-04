fa02d5a55f4361f900f9f44c653917c3d7718548
mwalimuianT <mwalimuiant@gmail.com>
2019-11-04 16:31:55 +0300

    added  alert, alert alerts the user message has been received

--- js/script.js
+++ js/script.js
@@ -9,8 +9,47 @@ $ (document).ready(function(){
         $("#prod").slideToggle();
     });
     $("#project1").hover(function(){
-        $("#text").show("slow")
+        $("#text1").toggle("slow");
+    
+    })
+    $("#project2").hover(function(){
+        $("#text2").toggle("slow");
+    
+    })
+    $("#project3").hover(function(){
+        $("#text3").toggle("slow");
+    
+    })
+    $("#project4").hover(function(){
+        $("#text4").toggle("slow");
+    
+    })
+    $("#project5").hover(function(){
+        $("#text5").toggle("slow");
+    
+    })
+    $("#project6").hover(function(){
+        $("#text6").toggle("slow");
+    
+    })
+    $("#project7").hover(function(){
+        $("#text7").toggle("slow");
+    
+    })
+    $("#project8").hover(function(){
+        $("#text8").toggle("slow");
     
     })
     });
+var submission=function(){
+ 
+var email=document.getElementById("email")
+if (email== "") {
+    alert("Please enter Email")
+}
+    else {
+     alert("Thank you for contacting us.")
+}
+event.preventDefault()
+}
 
